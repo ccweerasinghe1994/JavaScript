2c64b203baadf0650389c60756d80f108dd1dbbd
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-20 08:00:13 +0530

    initial commit

---
+++ 3.number/code.js
@@ -0,0 +1,12 @@
+var x = 1;
+console.log(typeof x);
+
+var y = 2.4;
+var z = 5.3e8;
+console.log(z);
+
+var max = Number.MAX_VALUE;
+console.log(max);
+var min = Number.MIN_VALUE;
+console.log(min);
+
