1087f8d94678c7f5c1a5d244e78ae479ab69bee1
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-20 10:50:20 +0530

    initial commit

---
+++ 4.speedlimitexsercise/code.js
@@ -0,0 +1,13 @@
+
+
+var speedlimitMiles;
+speedlimitMiles = 50;
+
+var speedlimitKilometers;
+
+speedlimitKilometers = speedlimitMiles*1.61;
+
+var speedFineMiles = speedlimitMiles * 1.1 + 2;
+
+var speedFineKilometers = speedFineMiles*1.61;
+console.log(speedlimitKilometers);
