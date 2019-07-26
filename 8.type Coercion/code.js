6afb62cf336bdb8f1ae99e35380e3b36b2f7613b
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-26 12:15:47 +0530

    init

---
+++ 8.type Coercion/code.js
@@ -0,0 +1,15 @@
+var z = "1" + 5;
+
+console.log(z);
+console.log(typeof z);
+
+var v = "1" - 5;
+console.log(v);
+console.log(typeof v);
+
+
+console.log(0 == false);
+console.log(0 == true);
+console.log(null == true);
+
+console.log(undefined == null);
