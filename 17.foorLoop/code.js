d3ea70444e14b944c3610aec50c67033fb5e0acb
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-27 07:34:06 +0530

    init

---
+++ 17.foorLoop/code.js
@@ -0,0 +1,20 @@
+// for (var i = 0; i < 100; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 7; i > 0; i--) {
+//   console.log(Math.pow(i, 2));
+// }
+
+// var shopping_list = ["braed", "milk", "eggs"];
+
+// for(var i = 0;i<shopping_list.length;i++){
+//     console.log(shopping_list[i])
+
+// }
+
+var number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (var i = 0; i < number_list.length; i++) {
+  number_list[i] += 10;
+  console.log(number_list[i]);
+}
