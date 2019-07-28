da11306e2010161fe7c93eec3cb6939f1b59fb8a
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-28 10:56:42 +0530

    init

---
+++ 18.ForrloopExcersize/code.js
@@ -0,0 +1,34 @@
+var hotel_1 = {
+  Hotel_name: "forest view hotel",
+  Price_per_night: 51,
+  Distance_from_the_center: 6.8
+};
+var hotel_2 = {
+  Hotel_name: "OYO Abby Hotel",
+  Price_per_night: 52,
+  Distance_from_the_center: 6.8
+};
+var hotel_3 = {
+  Hotel_name: "Camden Belmont",
+  Price_per_night: 69,
+  Distance_from_the_center: 6.8
+};
+var hotel_4 = {
+  Hotel_name: "St Athans Hotel",
+  Price_per_night: 74,
+  Distance_from_the_center: 6.8
+};
+var hotel_5 = {
+  Hotel_name: "st Giles London",
+  Price_per_night: 139,
+  Distance_from_the_center: 6.8
+};
+var hotel_6 = {
+  Hotel_name: "Ashburn Hotel",
+  Price_per_night: 153,
+  Distance_from_the_center: 6.8
+};
+
+var hotel = [hotel_1, hotel_2, hotel_3, hotel_4, hotel_5, hotel_6];
+
+var result = 'Here are the list of hotels avilable'
